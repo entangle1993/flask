d33b7dee8019ae4cfeb84be714f81d08610c28d2
なな <137413469@qq.com>
2019-05-19 23:54:41 +0800

    第7部分
    错误处理，部分语言汉化

--- app/forms.py
+++ app/forms.py
@@ -4,32 +4,43 @@ from wtforms.validators import DataRequired,ValidationError,Email,EqualTo,Length
 from app.models import User
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+    username = StringField('用户名', validators=[DataRequired()])
+    password = PasswordField('密码', validators=[DataRequired()])
+    remember_me = BooleanField('记住我')
+    submit = SubmitField('登录')
 
 #user register
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField('用户名', validators=[DataRequired()])
+    email = StringField('邮箱', validators=[DataRequired(), Email()])
+    password = PasswordField('密码', validators=[DataRequired()])
     password2 = PasswordField(
-        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Register')
+        '密码确认', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('注册')
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user is not None:
-            raise ValidationError('Please use a different username.')
+            raise ValidationError('该用户名已经被使用')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
-            raise ValidationError('Please use a different email address.')
+            raise ValidationError('该邮箱已经被使用')
 
 #about me表单
 class EditProfileForm(FlaskForm):
     username = StringField('名前', validators=[DataRequired()])
     about_me = TextAreaField('关于我', validators=[Length(min=0, max=140)])
-    submit = SubmitField('不要点！')
+    submit = SubmitField('提交')
+
+    #编辑时验证用户名
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('这个名字已经被抢先了Q.Q')
